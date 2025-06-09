3caadf6a28b6328fbffbd9b452f35c63cfeec3ae
as-anna <patty.asprer@gmail.com>
2025-06-08 17:28:23 -0700

    trying to fix images again

--- src/consts/CharacterColors.js
+++ src/consts/CharacterColors.js
@@ -3,324 +3,324 @@ export const CHARACTER_COLORS_MAP = {
         key: 'EICHI',
         name: 'Eichi',
         color: '#FFF3B8',
-        image: require('../images/EICHI.webp').default
+        image: require('../images/EICHI.webp')
     },
     WATARU: {
         key: 'WATARU',
         name: 'Wataru',
         color: '#A1D8E2',
-        image: require('../images/WATARU.webp').default
+        image: require('../images/WATARU.webp')
     },
     TORI: {
         key: 'TORI',
         name: 'Tori',
         color: '#F5B2B2',
-        image: require('../images/TORI.webp').default
+        image: require('../images/TORI.webp')
     },
     YUZURU: {
         key: 'YUZURU',
         name: 'Yuzuru',
         color: '#3E62AD',
-        image: require('../images/YUZURU.webp').default
+        image: require('../images/YUZURU.webp')
     },
     HOKUTO: {
         key: 'HOKUTO',
         name: 'Hokuto',
         color: '#0068B7',
-        image: require('../images/HOKUTO.webp').default
+        image: require('../images/HOKUTO.webp')
     },
     SUBARU: {
         key: 'SUBARU',
         name: 'Subaru',
         color: '#F3981D',
-        image: require('../images/SUBARU.webp').default
+        image: require('../images/SUBARU.webp')
     },
     MAKOTO: {
         key: 'MAKOTO',
         name: 'Makoto',
         color: '#65AB31',
-        image: require('../images/MAKOTO.webp').default
+        image: require('../images/MAKOTO.webp')
     },
     MAO: {
         key: 'MAO',
         name: 'Mao',
         color: '#941F57',
-        image: require('../images/MAO.webp').default
+        image: require('../images/MAO.webp')
     },
     TETORA: {
         key: 'TETORA',
         name: 'Tetora',
         color: '#302833',
-        image: require('../images/TETORA.webp').default
+        image: require('../images/TETORA.webp')
     },
     MIDORI: {
         key: 'MIDORI',
         name: 'Midori',
         color: '#00533F',
-        image: require('../images/MIDORI.webp').default
+        image: require('../images/MIDORI.webp')
     },
     SHINOBU: {
         key: 'SHINOBU',
         name: 'Shinobu',
         color: '#FFDC00',
-        image: require('../images/SHINOBU.webp').default
+        image: require('../images/SHINOBU.webp')
     },
     CHIAKI: {
         key: 'CHIAKI',
         name: 'Chiaki',
         color: '#E60033',
-        image: require('../images/CHIAKI.webp').default
+        image: require('../images/CHIAKI.webp')
     },
     KANATA: {
         key: 'KANATA',
         name: 'Kanata',
         color: '#008DB7',
-        image: require('../images/KANATA.webp').default
+        image: require('../images/KANATA.webp')
     },
     HIIRO: {
         key: 'HIIRO',
         name: 'Hiiro',
         color: '#BA2636',
-        image: require('../images/HIIRO.webp').default
+        image: require('../images/HIIRO.webp')
     },
     AIRA: {
         key: 'AIRA',
         name: 'Aira',
         color: '#FFF1CF',
-        image: require('../images/AIRA.webp').default
+        image: require('../images/AIRA.webp')
     },
     MAYOI: {
         key: 'MAYOI',
         name: 'Mayoi',
         color: '#522F60',
-        image: require('../images/MAYOI.webp').default
+        image: require('../images/MAYOI.webp')
     },
     TATSUMI: {
         key: 'TATSUMI',
         name: 'Tatsumi',
         color: '#7EBEA5',
-        image: require('../images/TATSUMI.webp').default
+        image: require('../images/TATSUMI.webp')
     },
     NAGISA: {
         key: 'NAGISA',
         name: 'Nagisa',
         color: '#A73836',
-        image: require('../images/NAGISA.webp').default
+        image: require('../images/NAGISA.webp')
     },
     HIYORI: {
         key: 'HIYORI',
         name: 'Hiyori',
         color: '#B8D200',
-        image: require('../images/HIYORI.webp').default
+        image: require('../images/HIYORI.webp')
     },
     IBARA: {
         key: 'IBARA',
         name: 'Ibara',
         color: '#74325C',
-        image: require('../images/IBARA.webp').default
+        image: require('../images/IBARA.webp')
     },
     JUN: {
         key: 'JUN',
         name: 'Jun',
         color: '#192F60',
-        image: require('../images/JUN.webp').default
+        image: require('../images/JUN.webp')
     },
     SHU: {
         key: 'SHU',
         name: 'Shu',
         color: '#E3ACAE',
-        image: require('../images/SHU.webp').default
+        image: require('../images/SHU.webp')
     },
     MIKA: {
         key: 'MIKA',
         name: 'Mika',
         color: '#006A6C',
-        image: require('../images/MIKA.webp').default
+        image: require('../images/MIKA.webp')
     },
     HINATA: {
         key: 'HINATA',
         name: 'Hinata',
         color: '#EB6EA0',
-        image: require('../images/HINATA.webp').default
+        image: require('../images/HINATA.webp')
     },
     YUTA: {
         key: 'YUTA',
         name: 'Yuta',
         color: '#00A1E9',
-        image: require('../images/YUTA.webp').default
+        image: require('../images/YUTA.webp')
     },
     RINNE: {
         key: 'RINNE',
         name: 'Rinne',
         color: '#B7282E',
-        image: require('../images/RINNE.webp').default
+        image: require('../images/RINNE.webp')
     },
     HIMERU: {
         key: 'HIMERU',
         name: 'HiMERU',
         color: '#89C3EB',
-        image: require('../images/HIMERU.webp').default
+        image: require('../images/HIMERU.webp')
     },
     KOHAKU: {
         key: 'KOHAKU',
         name: 'Kohaku',
         color: '#F4B3C2',
-        image: require('../images/KOHAKU.webp').default
+        image: require('../images/KOHAKU.webp')
     },
     NIKI: {
         key: 'NIKI',
         name: 'Niki',
         color: '#507EA5',
-        image: require('../images/NIKI.webp').default
+        image: require('../images/NIKI.webp')
     },
     REI: {
         key: 'REI',
         name: 'Rei',
         color: '#47266E',
-        image: require('../images/REI.webp').default
+        image: require('../images/REI.webp')
     },
     KAORU: {
         key: 'KAORU',
         name: 'Kaoru',
         color: '#FDD35C',
-        image: require('../images/KAORU.webp').default
+        image: require('../images/KAORU.webp')
     },
     KOGA: {
         key: 'KOGA',
         name: 'Koga',
         color: '#C9CACA',
-        image: require('../images/KOGA.webp').default
+        image: require('../images/KOGA.webp')
     },
     ADONIS: {
         key: 'ADONIS',
         name: 'Adonis',
         color: '#915DA3',
-        image: require('../images/ADONIS.webp').default
+        image: require('../images/ADONIS.webp')
     },
     TOMOYA: {
         key: 'TOMOYA',
         name: 'Tomoya',
         color: '#EEDCB3',
-        image: require('../images/TOMOYA.webp').default
+        image: require('../images/TOMOYA.webp')
     },
     NAZUNA: {
         key: 'NAZUNA',
         name: 'Nazuna',
         color: '#FFEC47',
-        image: require('../images/NAZUNA.webp').default
+        image: require('../images/NAZUNA.webp')
     },
     MITSURU: {
         key: 'MITSURU',
         name: 'Mitsuru',
         color: '#ED6D35',
-        image: require('../images/MITSURU.webp').default
+        image: require('../images/MITSURU.webp')
     },
     HAJIME: {
         key: 'HAJIME',
         name: 'Hajime',
         color: '#CAB8D9',
-        image: require('../images/HAJIME.webp').default
+        image: require('../images/HAJIME.webp')
     },
     KEITO: {
         key: 'KEITO',
         name: 'Keito',
         color: '#316745',
-        image: require('../images/KEITO.webp').default
+        image: require('../images/KEITO.webp')
     },
     KURO: {
         key: 'KURO',
         name: 'Kuro',
         color: '#E83929',
-        image: require('../images/KURO.webp').default
+        image: require('../images/KURO.webp')
     },
     SOUMA: {
         key: 'SOUMA',
         name: 'Souma',
         color: '#5654A2',
-        image: require('../images/SOUMA.webp').default
+        image: require('../images/SOUMA.webp')
     },
     IBUKI: {
         key: 'IBUKI',
         name: 'Ibuki',
         color: '#66BBCC',
-        image: require('../images/IBUKI.webp').default
+        image: require('../images/IBUKI.webp')
     },
     TSUKASA: {
         key: 'TSUKASA',
         name: 'Tsukasa',
         color: '#942343',
-        image: require('../images/TSUKASA.webp').default
+        image: require('../images/TSUKASA.webp')
     },
     LEO: {
         key: 'LEO',
         name: 'Leo',
         color: '#EC6D51',
-        image: require('../images/LEO.webp').default
+        image: require('../images/LEO.webp')
     },
     IZUMI: {
         key: 'IZUMI',
         name: 'Izumi',
         color: '#BBDBF3',
-        image: require('../images/IZUMI.webp').default
+        image: require('../images/IZUMI.webp')
     },
     RITSU: {
         key: 'RITSU',
         name: 'Ritsu',
         color: '#001E43',
-        image: require('../images/RITSU.webp').default
+        image: require('../images/RITSU.webp')
     },
     ARASHI: {
         key: 'ARASHI',
         name: 'Arashi',
         color: '#EDDE7B',
-        image: require('../images/ARASHI.webp').default
+        image: require('../images/ARASHI.webp')
     },
     NATSUME: {
         key: 'NATSUME',
         name: 'Natsume',
         color: '#D70035',
-        image: require('../images/NATSUME.webp').default
+        image: require('../images/NATSUME.webp')
     },
     TSUMUGI: {
         key: 'TSUMUGI',
         name: 'Tsumugi',
         color: '#00608D',
-        image: require('../images/TSUMUGI.webp').default
+        image: require('../images/TSUMUGI.webp')
     },
     SORA: {
         key: 'SORA',
         name: 'Sora',
         color: '#FFF352',
-        image: require('../images/SORA.webp').default
+        image: require('../images/SORA.webp')
     },
     MADARA: {
         key: 'MADARA',
         name: 'Madara',
         color: '#622D18',
-        image: require('../images/MADARA.webp').default
+        image: require('../images/MADARA.webp')
     },
     ESU: {
         key: 'ESU',
         name: 'Esu',
         color: '#80FFF4',
-        image: require('../images/ESU.webp').default
+        image: require('../images/ESU.webp')
     },
     KANNA: {
         key: 'KANNA',
         name: 'Kanna',
         color: '#BACCB8',
-        image: require('../images/KANNA.webp').default
+        image: require('../images/KANNA.webp')
     },
     YUME: {
         key: 'YUME',
         name: 'Yume',
         color: '#CCADD9',
-        image: require('../images/YUME.webp').default
+        image: require('../images/YUME.webp')
     },
     RAIKA: {
         key: 'RAIKA',
         name: 'Raika',
         color: '#298C7C',
-        image: require('../images/RAIKA.webp').default
+        image: require('../images/RAIKA.webp')
     }
 }
